9e8c3a86e53a9b502b6e7e0d019fe7994e17a264
jguedes <joao.guedes.da.neto@accenture.com>
2018-11-22 18:15:20 -0300

    criacao do resource relatorio mensal

---
+++ src/main/java/br/com/jguedes/teste/dbservice/resource/RelatorioMensalResource.java
@@ -0,0 +1,33 @@
+package br.com.jguedes.teste.dbservice.resource;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jguedes.teste.dbservice.entity.RelatorioMensal;
+import br.com.jguedes.teste.dbservice.repository.RelatorioMensalRepository;
+
+@RestController
+@RequestMapping(value="/rest/relatoriomensal")
+public class RelatorioMensalResource {
+	
+	@Autowired
+	private RelatorioMensalRepository relatorioMensalRepository;
+	
+	@GetMapping(value = "/all")
+	public List<RelatorioMensal> getAll(){
+		return relatorioMensalRepository.findAll();
+	}
+	
+	@PostMapping(value="/load")
+	public List<RelatorioMensal> persist(@RequestBody final RelatorioMensal relatorio){
+		relatorioMensalRepository.save(relatorio);
+		return relatorioMensalRepository.findAll();
+	}
+
+}
